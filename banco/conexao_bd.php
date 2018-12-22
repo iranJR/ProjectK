e29e2ef1ce47fe162104f24aa96c30840f7fdf20
iranf <iranfaefid@hotmail.com>
2018-12-22 14:18:25 -0200

    Commit do banco de dados e seus arquivos de conexão.

---
+++ banco/conexao_bd.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Iran Junior
+ * Date: 22/12/2018
+ * Time: 14:14
+ */
+
+require_once ("../banco/config_conexao.inc.php");
+try{
+    $pdo= new PDO(BANCO,USUARIO,SENHA);
+    $pdo->exec( "set names utf8");
+}
+catch (PDOException $erro){
+    echo"Erro ao tentar conectar com o banco de dados. ".$erro->getMessage();
+    echo exit(1);
+}
